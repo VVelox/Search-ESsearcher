c4e42617704018ce32d74c523c265b06b455b152
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2019-05-25 02:20:45 -0500

    add initial module stuff

---
+++ Search-ESsearcher/lib/Search/ESsearcher.pm
@@ -0,0 +1,109 @@
+package Search::ESsearcher;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Search::ESsearcher - The great new Search::ESsearcher!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Search::ESsearcher;
+
+    my $foo = Search::ESsearcher->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Zane C. Bowers-Hadley, C<< <vvelox at vvelox.net> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-search-essearcher at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=Search-ESsearcher>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Search::ESsearcher
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=Search-ESsearcher>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Search-ESsearcher>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/Search-ESsearcher>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/Search-ESsearcher>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2019 by Zane C. Bowers-Hadley.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0 (GPL Compatible)
+
+
+=cut
+
+1; # End of Search::ESsearcher
