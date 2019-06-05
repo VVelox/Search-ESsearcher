932fd35b5acf408c4c8df88145999946758d0357
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2019-06-05 01:09:17 -0500

    add postfix support

--- Search-ESsearcher/lib/Search/ESsearcher.pm
+++ Search-ESsearcher/lib/Search/ESsearcher.pm
@@ -17,11 +17,11 @@ Search::ESsearcher - Provides a handy system for doing templated elasticsearch s
 
 =head1 VERSION
 
-Version 0.2.0
+Version 0.3.0
 
 =cut
 
-our $VERSION = '0.2.0';
+our $VERSION = '0.3.0';
 
 
 =head1 SYNOPSIS
